e78a8bc7bbf8ac89ea160487372000eaba564494
Dawson Butts <dawsontbutts@gmail.com>
2022-03-07 18:30:15 -0700

    Made final changes and added launcher icon

--- app/src/main/java/com/example/quit/CreateGoalActivity.java
+++ app/src/main/java/com/example/quit/CreateGoalActivity.java
@@ -59,7 +59,7 @@ public class CreateGoalActivity extends AppCompatActivity {
         spinner = (Spinner) findViewById(R.id.measurements_spinner);
 // Create an ArrayAdapter using the string array and a default spinner layout
         arrayAdapter = ArrayAdapter.createFromResource(this,
-                R.array.measurements_array, android.R.layout.simple_spinner_item);
+                R.array.measurements_array, R.layout.my_custom_spinner);
 // Specify the layout to use when the list of choices appears
         arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 // Apply the adapter to the spinner
